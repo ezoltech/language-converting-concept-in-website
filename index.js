da7859517a4edec51f58d1b1d183ba4ec81617b9
ezoltech <ezramamo13@gmail.com>
2022-12-17 22:36:17 -0800

    first-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// english
+// The company itself is a very successful company. I followed
+//       should the pain be repelled? Having freed him from such conveniences as the pleasures of manners,
+//       let it be as if we were leading that by rejecting it, you should see the pleasure of life!
+//       Present?
+
+// Latin
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+//       repellendus dolor? Soluta illum quae commodi ut modi voluptates cumque,
+//       sit quasi ducimus quod reiciendis fugiat perspiciatis voluptas vitae id!
+//       Praesentium?
+
+// Amharic
+// ኩባንያው ራሱ በጣም ስኬታማ ኩባንያ ነው. ተከታተልኩ
+//       ህመሙ መቀልበስ አለበት? እንደ ሥነ ምግባራዊ ደስታ ካሉት ምቹ ሁኔታዎች ነፃ አውጥቶታል።
+//       እሱን በመናቅ የሕይወትን ደስታ እንድታዩ እንደመራን ይሁን።
+//       አለህ?
